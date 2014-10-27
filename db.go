67e6043f1d6af257fc44a0489e3daeaa874e4e1e
walu <imcnan@gmail.com>
2014-10-27 20:55:34 +0800

    upgrade .

---
+++ db.go
@@ -0,0 +1,254 @@
+package sqlutil
+
+import (
+	"database/sql"
+	"math/rand"
+	"strings"
+	"errors"
+	"fmt"
+
+	"github.com/erzha/elog"
+)
+
+var (
+	errArgsMatch = "err_args_not_match"
+	errDbQuery = "err_db_query"
+)
+
+type DB struct {
+	oriMaster *sql.DB
+	oriSlaves []*sql.DB
+
+}
+
+//create DB
+func NewDB(db *sql.DB) *DB {
+	ret := new(DB)
+	ret.oriMaster = db
+	ret.oriSlaves = []*sql.DB{db}
+	return ret
+}
+
+//create DB, master and slaves
+//
+//INSERT\Delete\Update use master,
+//Select use slaves randomly
+func NewRWDB(master *sql.DB, slaves ...*sql.DB) *DB {
+	ret := new(DB)
+	ret.oriMaster = master
+	ret.oriSlaves = slaves
+	return nil
+}
+
+/*
+
+help you converting map args to simple where args(eq relation only)
+
+Why slice was used default?
+
+becaulse golang's map is unordered
+
+like: {id:1, name:wgf}
+
+it will be sql:id=1 and name = wgf or sql:name = wgf and id =1, but the order is very important for database search.
+
+	//If you have to use the map, you can do it like this
+	queryParam := map[string]interface{}{
+		"id" : 1,
+		"name" : "wgf",
+	}
+	whereArgs := sqlutil.WhereMapToSlice(queryParam)
+	p.Select("wiki", whereArgs...)
+
+*/
+func WhereMapToSlice(where map[string]interface{}) []interface{} {
+	l := len(where)
+	if l == 0 {
+		return nil
+	}
+
+	ret := make([]interface{}, l*2)
+	i := 0
+	for k, v := range where {
+		ret[i] = k
+		ret[i+1] = v
+		i += 2
+	}
+	return ret
+}
+
+/*
+
+Simple select
+
+	p.Select("wiki")
+	select * from wiki
+
+	p.Select("wiki", "id", 1)
+	select * from wiki where id = 1
+
+	p.Select("wiki", "id", 1, "name", "wgf") 
+	select * from wiki where id = 1 and name = "wgf"
+
+	if args%2==1, return an error
+
+	selece one row only?
+	rs, _ := p.Select("wiki", "id", 1)
+	rs.Next()
+	......
+
+*/
+func (p *DB) Select(table string, args ...interface{}) (*Rows, error) {
+	var s, sqlWhere string
+	var queryArgs []interface{}
+	var err error
+
+	sqlWhere, queryArgs, err = parseWhereArgs(args)
+	if nil != err {
+		return nil, err
+	}
+
+	s = "SELECT * FROM " + table + sqlWhere
+	return p.Query(s, queryArgs...)
+}
+
+/*
+Delete rows
+
+args only support eq
+*/
+func (p *DB) Delete(table string, args ...interface{}) (sql.Result, error) {
+	var s, sqlWhere string
+	var queryArgs []interface{}
+	var err error
+
+	sqlWhere, queryArgs, err = parseWhereArgs(args)
+	if nil != err {
+		return nil, err
+	}
+
+	s = "DELETE FROM " + table + sqlWhere
+	return p.Exec(s, queryArgs...)
+}
+
+/*
+Insert rows
+*/
+func (p *DB) Insert(table string, row map[string]interface{}) (sql.Result, error) {
+	//INSERT INTO TABLE(FILEDS, FIELDS, FIELDS) VALUES()
+	var s string
+	var queryArgs []interface{}
+	var fields []string
+	var holders []string
+
+	l := len(row)
+	queryArgs = make([]interface{}, l)
+	fields = make([]string, l)
+	holders = make([]string, l)
+	s = "INSERT INTO " + table
+
+	i := 0
+	for k, v := range row {
+		fields[i] = k
+		holders[i] = "?"
+		queryArgs[i] = v
+		i++
+	}
+
+	s = fmt.Sprintf("INSERT INTO %s(%s) VALUES(%s)", table, strings.Join(fields, ", "), strings.Join(holders, ", "))
+	return p.Exec(s, queryArgs...)
+}
+
+//update table
+//whereArgs only support simple eq relation
+func (p *DB) Update(table string, row map[string]interface{}, whereArgs ...interface{}) (sql.Result, error) {
+	var s string
+	var queryArgs []interface{}
+	var fields []string
+
+	l := len(row)
+	queryArgs = make([]interface{}, l)
+	fields = make([]string, l)
+
+	i := 0
+	for k, v := range row {
+		fields[i] = k + " = ? "
+		queryArgs[i] = v
+		i++
+	}
+
+	sqlWhere, tmpArgs, e := parseWhereArgs(whereArgs)
+	if nil != e {
+		return nil, e
+	}
+	queryArgs = append(queryArgs, tmpArgs...)
+
+
+	s = fmt.Sprintf("UPDATE  %s SET %s %s", table, strings.Join(fields, ", "), sqlWhere)
+	return p.Exec(s, queryArgs...)
+}
+
+
+/*
+	query one row only?
+	rs, _ := p.Query()
+	rs.Next()
+	......
+*/
+func (p *DB) Query(s string, args ...interface{}) (*Rows, error) {
+	elog.DefaultLogger.Debug("query_sql_start ", s, args)
+	rs, err := p.db(s[0:6]).Query(s, args...)
+	elog.DefaultLogger.Debug("query_sql_done ", s, args, err)
+	if nil != err {
+		body := fmt.Sprintf("%s err:%v sql:%s args:%v", errDbQuery, err, s, args)
+		elog.DefaultLogger.Warning(body)
+		return nil, errors.New(body)
+	}
+
+	return NewRows(rs)
+}
+
+func (p *DB) Exec(s string, args ...interface{}) (sql.Result, error) {
+	return p.db(s[0:6]).Exec(s, args...)
+}
+
+func (p *DB) Close() error {
+	p.oriMaster.Close()
+	for i, _ := range p.oriSlaves {
+		p.oriSlaves[i].Close()
+	}
+	return nil
+}
+
+func (p *DB) db(firstSixChar string) *sql.DB {
+	if strings.ToUpper(firstSixChar) == "SELECT" {
+		index := rand.Intn(len(p.oriSlaves))
+		return p.oriSlaves[index]
+	} else {
+		return p.oriMaster
+	}
+}
+
+//if parse to where, there will be a space ahead
+func parseWhereArgs(where []interface{}) (string, []interface{}, error) {
+	l := len(where)
+	if l==0 {
+		return "", nil, nil
+	}
+
+	if l%2!=0 {
+		return "", nil, errors.New(fmt.Sprintf("%s %v", errArgsMatch, where))
+	}
+
+	var s string
+	var queryArgs []interface{}
+	queryArgs = make([]interface{}, l/2)
+
+	s = " WHERE "
+	for i:=0; i<l; i+=2 {
+		s += where[i].(string) + " = ? "
+		queryArgs[i/2] = where[i+1]
+	}
+
+	return s, queryArgs, nil
+}
---
+++ rows.go
@@ -0,0 +1,222 @@
+package sqlutil
+
+import (
+	"strconv"
+	"database/sql"
+	"errors"
+)
+
+/*
+Rows is instead of sql.Rows
+
+Rows will get all the result into memory of sql.Rows, 
+preventing you forget to call the Close() method, the reason let db conn can't be released.
+*/
+type Rows struct {
+	index int
+	dataLen int
+	currentData map[string]string
+	colnames []string
+
+	data []map[string]string
+}
+
+/*
+Rows will get all the result into memory of sql.Rows, 
+preventing you forget to call the Close() method, the reason let db conn can't be released.
+*/
+func NewRows(rs *sql.Rows) (*Rows, error) {
+	if nil == rs {
+		rs = new(sql.Rows)
+	}
+	defer rs.Close()
+
+	var err error
+	var tmp map[string]string
+
+	ret := &Rows{}
+	ret.currentData = make(map[string]string)
+	ret.data = make([]map[string]string, 0)
+	ret.colnames, err = rs.Columns()
+	if nil != err {
+		return nil, err
+	}
+
+	for rs.Next() {
+		tmp, err = fetchMap(rs)
+		if nil != err {
+			return nil, err
+		}
+
+		ret.data = append(ret.data, tmp)
+		ret.dataLen++
+	}
+	return ret, nil
+}
+
+func (rs *Rows) NumRows() int {
+	return rs.dataLen
+}
+
+func (rs *Rows) Next() bool {
+	if rs.index>=rs.dataLen {
+		return false
+	}
+
+	rs.currentData = rs.data[rs.index]
+	rs.index++
+	return true
+}
+
+func (rs *Rows) Columns() []string {
+	return rs.colnames
+}
+
+func (rs *Rows) FetchInt64(col string) (int64, error) {
+	return strconv.ParseInt(rs.currentData[col], 10, 64)
+}
+
+func (rs *Rows) FetchUint64(col string) (uint64, error) {
+	return strconv.ParseUint(rs.currentData[col], 10, 64)
+}
+
+func (rs *Rows) FetchInt32(col string) (ret int32, e error) {
+	var r int64
+	r, e = strconv.ParseInt(rs.currentData[col], 10, 32)
+	if nil != e {
+		ret = int32(r)
+	}
+	return
+}
+
+func (rs *Rows) FetchUint32(col string) (ret uint32, e error) {
+	var r uint64
+	r, e = strconv.ParseUint(rs.currentData[col], 10, 32)
+	if nil != e {
+		ret = uint32(r)
+	}
+	return
+}
+
+func (rs *Rows) FetchInt16(col string) (ret int16, e error) {
+	var r int64
+	r, e = strconv.ParseInt(rs.currentData[col], 10, 16)
+	if nil != e {
+		ret = int16(r)
+	}
+	return
+}
+
+
+func (rs *Rows) FetchUint16(col string) (ret uint16, e error) {
+	var r uint64
+	r, e = strconv.ParseUint(rs.currentData[col], 10, 16)
+	if nil != e {
+		ret = uint16(r)
+	}
+	return
+}
+
+
+func (rs *Rows) FetchInt8(col string) (ret int8, e error) {
+	var r int64
+	r, e = strconv.ParseInt(rs.currentData[col], 10, 8)
+	if nil != e {
+		ret = int8(r)
+	}
+	return
+}
+
+
+func (rs *Rows) FetchUint8(col string) (ret uint8, e error) {
+	var r uint64
+	r, e = strconv.ParseUint(rs.currentData[col], 10, 8)
+	if nil != e {
+		ret = uint8(r)
+	}
+	return
+}
+
+func (rs *Rows) FetchInt(col string) (ret int, e error) {
+	var r int64
+	r, e = strconv.ParseInt(rs.currentData[col], 10, 64)
+	if nil != e {
+		ret = int(r)
+	}
+	return
+}
+
+func (rs *Rows) FetchUint(col string) (ret uint, e error) {
+	var r uint64
+	r, e = strconv.ParseUint(rs.currentData[col], 10, 64)
+	if nil != e {
+		ret = uint(r)
+	}
+	return
+}
+
+func (rs *Rows) FetchFloat32(col string) (ret float32, e error) {
+	var r float64
+	r, e = strconv.ParseFloat(rs.currentData[col], 64)
+	if nil != e {
+		ret = float32(r)
+	}
+	return
+}
+
+func (rs *Rows) FetchFloat64(col string) (float64, error) {
+	return strconv.ParseFloat(rs.currentData[col], 64)
+}
+
+func (rs *Rows) FetchBool(col string) (bool, error) {
+	return strconv.ParseBool(rs.currentData[col])
+}
+
+func (rs *Rows) FetchString(col string) (string, error) {
+	return rs.currentData[col], nil
+}
+
+func (rs *Rows) Fetch() map[string]string {
+	return rs.currentData
+}
+
+/*
+func (rs *Rows) FetchRawBytes(col string) (ret sql.RawBytes, e error) {
+}
+*/
+
+func fetchMap(rs *sql.Rows) (map[string]string, error) {
+	var colnames []string
+	var err	error
+
+	colnames, err = rs.Columns()
+	if nil != err {
+		return nil, errors.New("rows to map error: " + err.Error())
+	}
+
+	var lenCol int
+	lenCol = len(colnames)
+
+	var ret map[string]string
+	ret = make(map[string]string)
+
+	var args []sql.RawBytes
+	var scanArgs []interface{}
+
+	args = make([]sql.RawBytes, lenCol)
+	scanArgs = make([]interface{}, lenCol)
+	for i := range args {
+		scanArgs[i] = &args[i]
+	}
+
+	err = rs.Scan(scanArgs...)
+	if nil != err {
+		return nil, err
+	}
+
+	for index, val := range args {
+		ret[colnames[index]] = string(val)
+	}
+
+	return ret, nil
+}
